4e26c7312a5b12f09b9ebfdc066fd25203fe62ba
Roman <papachiv.roman@gmail.com>
2017-11-03 17:51:21 +0200

    home page + icons

--- gulpfile.js
+++ gulpfile.js
@@ -55,19 +55,17 @@ gulp.task('css-libs', ['sass'], function() {
 
 gulp.task('sprite', function () {
     // Generate our spritesheet
-    var spriteData = gulp.src('app/img/icons/*.png')
-        .pipe(spritesmith({
-            imgName: 'sprite.png',
-            cssName: '_sprite.scss',
-            padding:5
-        }));
+    var spriteData = gulp.src('app/img/icons/*.png').pipe(spritesmith({
+        imgName: 'sprite.png',
+        cssName: '_sprite.scss'
+    }));
 
     // Pipe image stream through image optimizer and onto disk
     var imgStream = spriteData.img
     // DEV: We must buffer our stream into a Buffer for `imagemin`
-    //     .pipe(buffer())
-    //     .pipe(imagemin())
-        .pipe(gulp.dest('app/img'));
+        //.pipe(buffer())
+        //.pipe(imagemin())
+        .pipe(gulp.dest('app/img/sprites/'));
 
     // Pipe CSS stream through CSS optimizer and onto disk
     var cssStream = spriteData.css
