cdf1847727833c19e9c2519e9daa7dbc8078e713
Roman <papachiv.roma@gmail.com>
2018-01-20 20:14:30 +0200

    copy from new_architec

--- gulpfile.js
+++ gulpfile.js
@@ -1,125 +1,246 @@
-var gulp            = require('gulp'),
-    sass            = require('gulp-sass'),
-    browserSync     = require('browser-sync'),
-    concat          = require('gulp-concat'),
-    uglify          = require('gulp-uglify'),
-    rename          = require('gulp-rename'),
-    cssnano         = require('gulp-cssnano'),
-    del             = require('del'),
-    autoprefixer    = require('gulp-autoprefixer'),
-	svgSprite         = require('gulp-svg-sprites'),
-    svgmin          = require('gulp-svgmin'),
-    cheerio         = require('gulp-cheerio'),
-    replace         = require('gulp-replace'),
-    fs              = require('fs'),
-    spritesmith     = require('gulp.spritesmith'),
-    //phantomjssmith  = require('phantomjssmith');,
-    merge           = require('merge-stream'),
-    buffer          = require('vinyl-buffer'),
-    imagemin        = require('gulp-imagemin'),
-    csso            = require('gulp-csso'),
-    sourcemaps      = require('gulp-sourcemaps');
+var gulp = require('gulp'),
+    autoprefixer = require('gulp-autoprefixer'),
+    //watch = require('gulp-watch'),
 
-gulp.task('sass', function(){
-    return gulp.src('app/sass/**/*.scss')
-        //.pipe(sourcemaps.init())
-        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-        .pipe(autoprefixer(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: false }))
-        //.pipe(sourcemaps.write('../css', {addComment: false}))
-        .pipe(gulp.dest('app/css'))
+	////Minify JavaScript
+    uglify = require('gulp-uglify'),
+
+	////Compile scss
+    sass = require('gulp-sass'),
+    sourcemaps = require('gulp-sourcemaps'),
+
+	/////Take all template
+    //rigger = require('gulp-rigger'),
+    //cssmin = require('gulp-clean-css'),
+
+    cssnano = require('gulp-cssnano'),
+
+	/////Compile pug files
+    pug = require('gulp-pug'),
+
+	////Optimize images
+    imagemin = require('gulp-imagemin'),
+    imageminJpegtran = require('imagemin-jpegtran'),
+    pngquant = require('imagemin-pngquant'),
+
+
+    browserSync = require('browser-sync'),
+
+    concat = require('gulp-concat'),
+
+    rename = require('gulp-rename'),
+
+	////Cleaning directories
+    rimraf = require('rimraf'),
+
+    ////Generate svg sprite
+	svgSprite = require('gulp-svg-sprites'),
+	filter    = require('gulp-filter'),
+	svg2png   = require('gulp-svg2png'),
+    svgmin = require('gulp-svgmin'),
+    cheerio = require('gulp-cheerio'),
+    replace = require('gulp-replace'),
+    fs = require('fs'),
+
+	////Generate png sprite
+    spritesmith = require('gulp.spritesmith'),
+	merge = require('merge-stream'),
+    buffer = require('vinyl-buffer')
+
+
+var buildFolder = 'build',
+	srcFolder = 'src',
+	path = {
+		build: { //Where gulp must build project
+			html: buildFolder +'/',
+			js: buildFolder +'/js/',
+			css: buildFolder +'/css/',
+			img: buildFolder +'/images/**/*.*',
+			fonts: buildFolder +'/fonts/'
+		},
+		src: { //From where gulp must take sources
+			pugFiles: srcFolder +'/*.pug',
+			js: srcFolder +'/js/*.js',
+			style: srcFolder +'/scss/',
+			img: [
+				srcFolder +'/images/**/*',
+				!srcFolder +'/images/icons'
+			],
+			icons: srcFolder +'/images/icons/',
+			// iconsPng: srcFolder +'/images/icons/iconsPng/*',
+			fonts: srcFolder +'/fonts/**/*.*'
+		},
+		watch: { //Тут мы укажем, за изменением каких файлов мы хотим наблюдать
+            pugFiles: srcFolder +'/**/*.pug',
+			js: srcFolder +'/js/**/*.js',
+			style: srcFolder +'/scss/**/*.scss',
+			img: srcFolder +'/images/**/*.*',
+			//fonts: srcFolder +'/fonts/**/*.*'
+		},
+		clean: './' + buildFolder
+	};
+
+var ServerConfig = {
+    server: {
+        baseDir:'./' + buildFolder
+    },
+    // tunnel: true,
+    host: 'localhost',
+    //port: 3000
+};
+
+
+gulp.task('html:build', function buildHTML() {
+    return gulp.src(path.src.pugFiles)
+        .pipe(pug({
+            pretty:true,
+            filename: true
+        }))
+        .pipe(gulp.dest(path.build.html)) //Put result into build directory
         .pipe(browserSync.reload({stream: true}))
 });
 
-gulp.task('browser-sync',function(){
-    browserSync({
-        server: "./app"
-    });
-});
-
-gulp.task('scripts', function() {
+gulp.task('libsJs:build', function() {//scripts
     return gulp.src([
-        // 'app/libs/modernizr.js',
-        'app/libs/jquery/dist/jquery.min.js',
-        'app/libs/tether/dist/js/tether.min.js',
-        'app/libs/bootstrap/dist/js/bootstrap.min.js',
-        'app/libs/OwlCarousel2/dist/owl.carousel.min.js',
-        'app/libs/carousel-2rows/carousel-2rows.js',
-        'app/libs/gsap/src/minified/TweenMax.min.js',
-        'app/libs/seiyria-bootstrap-slider/dist/bootstrap-slider.js',
-        'app/libs/dropzone/dist/dropzone.js',
-        'app/libs/chosen/chosen.jquery.js',
-        'app/libs/mobile-detect/mobile-detect.js',
-        'app/libs/ThreeDots/ThreeDots.js',
-        'app/libs/sprite-cash.js'
+        srcFolder + '/libs/jquery/dist/jquery.min.js',
+        srcFolder + '/libs/tether/dist/js/tether.min.js',
+        srcFolder + '/libs/bootstrap/dist/js/bootstrap.min.js',
+        srcFolder + '/libs/OwlCarousel2/dist/owl.carousel.min.js',
+        srcFolder + '/libs/gsap/src/minified/TweenMax.min.js',
+        srcFolder + '/libs/seiyria-bootstrap-slider/dist/bootstrap-slider.js',
+        srcFolder + '/libs/dropzone/dist/dropzone.js',
+        srcFolder + '/libs/chosen/chosen.jquery.js',
+        srcFolder + '/libs/mobile-detect/mobile-detect.js',
+        srcFolder + '/libs/ThreeDots/ThreeDots.js',
+        srcFolder + '/libs/sprite-cash.js'
     ])
-    .pipe(concat('libs.min.js'))
-    .pipe(uglify())
-    .pipe(gulp.dest('app/js'));
+	.pipe(concat('libs.min.js'))
+	.pipe(gulp.dest(srcFolder + '/js'));
 });
 
-gulp.task('css-libs', ['sass'], function() {
-    return gulp.src('app/css/libs.css')
-    .pipe(cssnano())
-    .pipe(rename({suffix: '.min'}))
-    .pipe(gulp.dest('app/css'));
+gulp.task('js:build', function () {
+    gulp.src(path.src.js)
+	.pipe(uglify()) //Optimize over js files
+	.pipe(gulp.dest(path.build.js))
+	.pipe(browserSync.reload({stream: true}))
 });
 
-gulp.task('sprite', function () {
-    // Generate our spritesheet
-    var spriteData = gulp.src('app/images/icons/*.png').pipe(spritesmith({
-        imgName: 'sprite.png',
-        cssName: '_sprite.scss'
-    }));
-
-    // Pipe image stream through image optimizer and onto disk
-    var imgStream = spriteData.img
-    // DEV: We must buffer our stream into a Buffer for `imagemin`
-        //.pipe(buffer())
-        //.pipe(imagemin())
-        .pipe(gulp.dest('app/images/sprites/'));
-
-    // Pipe CSS stream through CSS optimizer and onto disk
-    var cssStream = spriteData.css
-        //.pipe(csso())
-        .pipe(gulp.dest('app/sass/'));
-
-    // Return a merged stream to handle both `end` events
-    return merge(imgStream, cssStream);
+gulp.task('style:build', function () {
+    gulp.src(path.src.style + 'main.scss') //Выберем наш main.scss
+	.pipe(sourcemaps.init()) //То же самое что и с js
+	.pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+	.pipe(autoprefixer(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: false }))
+	.pipe(cssnano()) //Сожмем
+	.pipe(sourcemaps.write())
+	.pipe(gulp.dest(path.build.css)) //И в build
+	.pipe(browserSync.reload({stream: true}))
 });
+
+gulp.task('libsCss:build', function () {
+    gulp.src(path.src.style + '/libs.scss')
+	.pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+	.pipe(cssnano())
+	.pipe(rename({suffix: '.min'}))
+	.pipe(gulp.dest(buildFolder + '/css'));
+});
+
+gulp.task('fonts:build', function() {
+    gulp.src(path.src.fonts)
+	.pipe(gulp.dest(path.build.fonts))
+});
+
+gulp.task('image:build', function () {
+    gulp.src(path.src.img) //Выберем наши картинки
+        .pipe(imagemin({ //Сожмем их
+            progressive: true,
+            svgoPlugins: [{removeViewBox: false}],
+            use: [pngquant()],
+            interlaced: true
+        }))
+        .pipe(gulp.dest(path.build.img)) //И бросим в build
+        .pipe(browserSync.reload({stream: true}))
+});
+
+gulp.task('build', [
+    'html:build',
+    'libsJs:build',
+    'js:build',
+    'style:build',
+    'libsCss:build',
+    'fonts:build',
+    'image:build'
+]);
+
+// gulp.task('sprite', function () {
+//     // Generate our spritesheet
+//     var spriteData = gulp.src('app/images/icons/*.png').pipe(spritesmith({
+//         imgName: 'sprite.png',
+//         cssName: '_sprite.scss'
+//     }));
+//
+//     // Pipe image stream through image optimizer and onto disk
+//     var imgStream = spriteData.img
+//     // DEV: We must buffer our stream into a Buffer for `imagemin`
+//         .pipe(buffer())
+//         .pipe(imagemin())
+//         .pipe(gulp.dest('app/images/sprites/'));
+//
+//     // Pipe CSS stream through CSS optimizer and onto disk
+//     var cssStream = spriteData.css
+//         //.pipe(csso())
+//         .pipe(gulp.dest('app/scss/'));
+//
+//     // Return a merged stream to handle both `end` events
+//     return merge(imgStream, cssStream);
+// });
 
 gulp.task('svgSpriteBuild', function () {
-	return gulp.src('app/images/icons-svg/*.svg')
-	// minify svg
-	.pipe(svgmin({
-		js2svg: {
-			pretty: true
-		}
-	}))
-	//remove all fill and style declarations in out shapes
-	.pipe(cheerio({
-		run: function ($) {
-			$('[fill]').removeAttr('fill');
-			$('[style]').removeAttr('style');
-		},
-		parserOptions: { xmlMode: true }
-	}))
-	// cheerio plugin create unnecessary string '>', so replace it.
-	.pipe(replace('&gt;', '>'))
-	// build svg sprite
-	.pipe(svgSprite({
-				mode: "symbols",
-				preview: false,
-				selector: "icon-%f",
-				layout: "vertical",
-				svg: {
-					symbols: 'symbol_sprite.html'
-				}
-			}
-	))
-	.pipe(gulp.dest('app/'));
+	return gulp.src(path.src.icons + '/iconsSvg/*')
+		// .pipe(svgmin({
+		// 	js2svg: {
+		// 		pretty: true
+		// 	}
+		// }))
+		//remove all fill and style declarations in out shapes
+		// .pipe(cheerio({
+		// 	run: function ($) {
+		// 		$('[fill]').removeAttr('fill');
+		// 		$('[style]').removeAttr('style');
+		// 	},
+		// 	parserOptions: { xmlMode: true }
+		// }))
+		// cheerio plugin create unnecessary string '>', so replace it.
+		//.pipe(replace('&gt;', '>'))
+		// build svg sprite
+		.pipe(svgSprite({
+            cssFile: '_spriteSvg.scss',
+            preview: false,
+            svg: {
+                sprite: "../../images/sprites/spriteSvg.svg"
+            },
+            svgPath: '../../images/sprites/spriteSvg.svg',
+            pngPath: '../../images/sprites/spriteSvg.png',
+            layout: "vertical",
+            padding: 10,
+            templates: { scss: true },
+            // templates: {
+            //     css: fs.readFileSync( ''+path.src.style +'sprites/_sprite-template.scss', "utf-8")
+            // }
+				//mode: "symbols",
+				//preview: false,
+				//selector: "icon-%f",
+				//layout: "vertical",
+				// svg: {
+				// 	symbols: 'symbol_sprite.html'
+				// }
+		}))
+		.pipe(gulp.dest(path.src.style + '/sprites/')) // Write the sprite-sheet + CSS + Preview
+		.pipe(filter(path.src.img + '/icons/iconsSvg/*'))  // Filter out everything except the SVG file
+		.pipe(svg2png())           // Create a PNG
+		.pipe(gulp.dest(path.src.img + '/sprites/'))
 });
 
-// create sass file for our sprite
+// create scss file for our sprite
 gulp.task('svgSpriteSass', function () {
 	return gulp.src('app/images/icons-svg/*.svg')
 	.pipe(svgSprite({
@@ -128,9 +249,9 @@ gulp.task('svgSpriteSass', function () {
 				svg: {
 					sprite: 'svg_sprite.html'
 				},
-				cssFile: 'sass/_svg_sprite.scss',
+				cssFile: 'scss/_svg_sprite.scss',
 				templates: {
-					css: fs.readFileSync('app/sass/_sprite-template.scss', "utf-8")
+					css: fs.readFileSync('app/scss/_sprite-template.scss', "utf-8")
 				}
 			}
 	))
@@ -139,12 +260,56 @@ gulp.task('svgSpriteSass', function () {
 
 gulp.task('svgSprite', ['svgSpriteBuild', 'svgSpriteSass']);
 
-gulp.task('watch', ['browser-sync', 'css-libs', 'scripts'], function() {
-    gulp.watch('app/sass/**/*.scss', ['sass']);
-    gulp.watch('app/*.html', browserSync.reload);
-    gulp.watch('app/js/**/*.js', browserSync.reload);
+// gulp.task('watch', function(){
+//     watch([path.watch.html], function(event, cb) {
+//         gulp.start('html:build')
+// 		//.pipe(browserSync.reload({stream: true}))
+//     });
+//     watch([path.watch.style], function(event, cb) {
+//         gulp.start('style:build')
+// 		//.pipe(browserSync.reload({stream: true}))
+//     });
+//     // watch([path.watch.js], function(event, cb) {
+//     //     gulp.start('libsJs:build');
+//     // });
+//     watch([path.watch.js], function(event, cb) {
+//         gulp.start('js:build')
+//         //.pipe(browserSync.reload({stream: true}))
+//     });
+//     watch([path.watch.img], function(event, cb) {
+//         gulp.start('image:build')
+// 		//.pipe(browserSync.reload({stream: true}))
+//     });
+//     watch([path.watch.fonts], function(event, cb) {
+//         gulp.start('fonts:build')
+// 		//.pipe(browserSync.reload({stream: true}))
+//     });
+// });
+
+gulp.task('watch', function() {
+    gulp.watch(path.watch.pugFiles, ['html:build']);
+    gulp.watch(path.watch.style, ['style:build']);
+    gulp.watch(path.watch.js, ['js:build']);
+    gulp.watch(path.watch.img, ['image:build']);
 });
+
+gulp.task('webserver', function () {
+    browserSync(ServerConfig);
+});
+
+gulp.task('clean', function (cb) {
+    rimraf(path.clean, cb);
+});
+
+// gulp.task('watch', ['browser-sync', 'css-libs', 'scripts'], function() {
+//     gulp.watch('app/scss/**/*.scss', ['sass']);
+//     gulp.watch('app/*.html', browserSync.reload);
+//     gulp.watch('app/js/**/*.js', browserSync.reload);
+// });
 
 // ----------------------Дефолтный таск gulp --------------------------------
 
-gulp.task('default', ['watch']);
+//gulp.task('default', ['watch']);
+gulp.task('default', ['build', 'webserver', 'watch']);
+gulp.task('watch-spriteSvg', ['svgSprite', 'build', 'webserver', 'watch']);
+gulp.task('watch-spritePng', ['sprite', 'build', 'webserver', 'watch']);
